eb87714f11d136b10dabbcc3db4bddc283cfa8b7
Schveitzer <alan.schveitzer@gmail.com>
2016-03-08 23:45:40 -0300

    com number

--- teste/src/teste/AreadoQuadrado.java
+++ teste/src/teste/AreadoQuadrado.java
@@ -6,12 +6,11 @@ public class AreadoQuadrado {
 		
 		Scanner s1 = new Scanner(System.in);
 		int lado = s1.nextInt();
-		System.out.println(area(lados));
+		System.out.println(area(lados25));
 	}
 	
 	public static int area (int lado) {
 		return lado * lado;
-		//comentário lol
-		//mais um com uma bugada lalalala
+		
 	}
 }
