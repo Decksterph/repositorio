445ef07ddbaac9a05ab9d89f9ee0cb60062033e7
Schveitzer <alan.schveitzer@gmail.com>
2016-03-08 23:44:54 -0300

    erro

--- teste/src/teste/AreadoQuadrado.java
+++ teste/src/teste/AreadoQuadrado.java
@@ -6,7 +6,7 @@ public class AreadoQuadrado {
 		
 		Scanner s1 = new Scanner(System.in);
 		int lado = s1.nextInt();
-		System.out.println(area(lado));
+		System.out.println(area(lados));
 	}
 	
 	public static int area (int lado) {
