2e8dd62131ab963175c2b88f93d72f2e781a16ff
Decksterph <alan.schveitzer@hotmail.com>
2016-03-09 10:21:03 -0300

    LAalalalal

--- casa/src/casa/quit.java
+++ casa/src/casa/quit.java
@@ -2,6 +2,6 @@ package casa;
 
 public class quit {
 	public static void main (String[] args){
-		System.out.println("olá Mundo, 35 anos e vai indo");
+		System.out.println("olá Mundo, 35 anos e vai indo Bem");
 	}
 }
